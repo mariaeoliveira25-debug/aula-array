e194d4bd9ab9df707f1d13f710bcd4454e35091a
Maria Eduarda Rosa <maria.e.oliveira25@edu.senai.br>
2026-05-21 14:48:33 -0300

    exemplos array

---
+++ exemplos/exemplos.js
@@ -0,0 +1,36 @@
+  // Exemplo de array 
+  
+  let sucos = ("maçã", "banana", "laranja", "uva");
+
+  // Criando um array de números 
+  let numeros = [1, 2, 3, 4, 5];
+  console.log(numeros[2]);
+  
+   // Acessando um elemento especifico do array 
+   let animais = ["cachorro", "Gatos", "Passaro", "Peixe"];
+   console.log(animais[1]);
+
+    //Alterando um elemneto do array
+    let times = ["Flamengo","Palemiras", "São Paulo ", "Corinthians"];
+    times[2]= "Vasco";
+    console.log(times);
+    
+    // Descobrindo o tamanho do array 
+    console.log(games.length);
+
+    // Adicionando um elemento ao final do array
+    let cores = ["Vermelho","Azul", "Verde"];
+
+    // Removendo o último elemento do array
+    let carros = [ "Fusca", "Gol", "Civic", "Corolla"];
+    carros.pop();
+    console.log(carros);
+
+    // Removendo o primeiro elemento do array
+    let cidades = ["São Paulo", "Rio d janeiro", "Belo Horizonte", "Curitiba "];
+    cidades.shift();
+    console.log (cidades);
+
+    //Adicionando um elemento no inicio do array 
+    let países = ["Brasil", "Argentina", "Chile"];
+    console.log(países);
